472a0585ab6cd7bd576f3168c2f3615d69d97e77
kysclient <kysclient@gmail.com>
2023-06-28 15:11:48 +0900

    refactor: nextjs cors config

--- next.config.js
+++ next.config.js
@@ -18,6 +18,27 @@ const nextConfig = {
     };
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)', // CORS를 적용할 경로 지정
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // 모든 도메인을 허용하려면 '*'를 사용
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, PATCH, OPTIONS', // 허용할 HTTP 메서드 지정
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Authorization, Content-Type', // 허용할 헤더 지정
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
