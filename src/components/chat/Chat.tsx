85061151159e63da85255f665f8928ddcbcd78d1
kysclient <kysclient@gmail.com>
2023-06-30 10:43:11 +0900

    config baseURL

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -123,7 +123,7 @@ export const Chat = memo(({stopConversationRef, closeModal, open, openModal}: Pr
                 prompt: message.content,
                 temperature: 1.0,
             };
-            const endpoint = 'api/aichat'
+            const endpoint = `${process.env.NEXT_PUBLIC_URL}/api/aichat`
             let body = JSON.stringify(chatBody);
             const controller = new AbortController();
             const response = await fetch(endpoint, {
