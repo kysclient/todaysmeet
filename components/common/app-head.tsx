10e1e73d03e8ced00dac13b2076ccca63dea4e8d
kysclient <kysclient@gmail.com>
2023-07-21 16:35:41 +0900

    update favicon

--- components/common/app-head.tsx
+++ components/common/app-head.tsx
@@ -5,8 +5,8 @@ export function AppHead(): JSX.Element {
         <Head>
             <title>투밋 - 오늘의만남</title>
             <meta name='og:title' content='투밋 - 오늘의만남'/>
-            <link rel='icon' href='/public/favicon.ico'/>
-            <link rel='manifest' href='/public/site.webmanifest' key='site-manifest'/>
+            <link rel='icon' href='/favicon.ico'/>
+            <link rel='manifest' href='/site.webmanifest' key='site-manifest'/>
             <meta name='todaysmeet:site' content='@kysclient'/>
             <meta name='todaysmeet:card' content='summary_large_image'/>
             <meta name="viewport"
