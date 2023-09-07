6c3cd1e0df1aafe5e1194c3a4e5d57f149546459
kysclient <kysclient@gmail.com>
2023-09-07 12:00:47 +0900

    refactor: update metadata

--- components/common/app-head.tsx
+++ components/common/app-head.tsx
@@ -5,7 +5,7 @@ export function AppHead(): JSX.Element {
         <Head>
             <title>투밋 - 오늘의만남</title>
             <meta name='og:title' content='오늘의만남 - 투밋'/>
-            <link rel='icon' href="/logo512.png"/>
+            <link rel='icon' href="/logo/logo-toss-symbol-alpha.png"/>
             <link rel='manifest' href='/site.webmanifest' key='site-manifest'/>
             <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png"/>
             <link rel="apple-touch-icon" sizes="512x512" href="/logo512.png"/>
